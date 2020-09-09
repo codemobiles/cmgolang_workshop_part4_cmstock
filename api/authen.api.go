d40e304bf3efbff90ad116b18f7ebba8999e13aa
codemobiles <chaiyasit.t@gmail.com>
2020-09-09 15:42:16 +0700

    ep9 - hash password with bcrypt

--- api/authen.api.go
+++ api/authen.api.go
@@ -5,6 +5,7 @@ import (
 	"main/model"
 	"time"
 	_ "time"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,7 +25,7 @@ func login(c *gin.Context) {
 func register(c *gin.Context) {
 	var user model.User
 	if c.ShouldBind(&user) == nil {		
-		// user.Password, _ = hashPassword(user.Password)
+		user.Password, _ = hashPassword(user.Password)
 		user.CreatedAt = time.Now()						
 		if err := db.GetDB().Create(&user).Error; err != nil {			
 			c.JSON(200, gin.H{"result": "nok", "error": err})
@@ -34,4 +35,16 @@ func register(c *gin.Context) {
 	} else {
 		c.JSON(401, gin.H{"status": "unable to bind data"})
 	}		
+}
+
+
+
+func checkPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func hashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	gorm.io/driver/sqlite v1.1.1
 	gorm.io/gorm v1.20.0
 )
