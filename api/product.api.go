cf9a788703a5b0ae4346ec949cc1e8e006079770
codemobiles <chaiyasit.t@gmail.com>
2020-09-11 07:37:24 +0700

    ep11 - interceptor

--- api/product.api.go
+++ api/product.api.go
@@ -1,11 +1,26 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func myInterceptor(c *gin.Context){
+	token := c.Query("token")
+	if (token == "1234"){
+		c.Next()
+	}else{
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+		c.Abort()
+	}
+}
 
 func SetupProductAPI(router *gin.Engine) {
 	productAPI := router.Group("/api/v2")
 	{
-		productAPI.GET("/product", getProduct)
+		productAPI.GET("/product",myInterceptor, getProduct)
 		productAPI.POST("/product", createProduct)
 	}
 }
