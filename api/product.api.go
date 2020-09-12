4c30f453d0b9451426f7879f79b6551bf4215987
codemobiles <chaiyasit.t@gmail.com>
2020-09-12 14:48:02 +0700

    ep17 - multiple-part create product with file

--- api/product.api.go
+++ api/product.api.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"fmt"
 	"main/interceptor"
 	"main/model"
+	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -28,7 +31,12 @@ func createProduct(c *gin.Context) {
 	product.Stock, _ = strconv.ParseInt(c.PostForm("stock"), 10, 64)
 	product.Price, _ = strconv.ParseFloat(c.PostForm("price"), 64)
 	image, _ := c.FormFile("image")	
-	product.Image = image.Filename
+	product.Image = image.Filename	
+
+	runningDir, _ := os.Getwd()		
+	filePath :=  fmt.Sprintf("%s/uploaded/images/%s", runningDir, image.Filename)
+	c.SaveUploadedFile(image, filePath)
+
 	c.JSON(200, gin.H{"result": product})
 
 }
