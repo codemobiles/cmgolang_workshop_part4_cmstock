e5be196fbdd2df0c0db0c8eaf49281749bee1984
codemobiles <chaiyasit.t@gmail.com>
2020-09-20 11:44:32 +0700

    ep30 - switch database driver to mysql

--- db/db.go
+++ db/db.go
@@ -3,7 +3,7 @@ package db
 import (
 	"main/model"
 
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -17,7 +17,13 @@ func GetDB() *gorm.DB {
 // SetupDB - setup dabase for sharing to all api
 func SetupDB() {
 
-	database, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	// sqlite "gorm.io/driver/sqlite"
+	// database, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+
+	// mysql "gorm.io/driver/mysql"
+	dsn := "root:@tcp(127.0.0.1:3306)/cmgostock?charset=utf8mb4&parseTime=True&loc=Local"
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		panic("failed to connect database")
 	}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	gorm.io/driver/mysql v1.0.1
 	gorm.io/driver/sqlite v1.1.1
 	gorm.io/gorm v1.20.0
 )
