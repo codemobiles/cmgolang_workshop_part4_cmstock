19696a0816fdfd07ecd43943d03bce7a2451836d
codemobiles <chaiyasit.t@gmail.com>
2020-09-21 08:38:44 +0700

    ep32 - switch to postgresql

--- db/db.go
+++ db/db.go
@@ -3,7 +3,7 @@ package db
 import (
 	"main/model"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -18,11 +18,15 @@ func GetDB() *gorm.DB {
 func SetupDB() {
 
 	// sqlite "gorm.io/driver/sqlite"
-	// database, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	database, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
 
 	// mysql "gorm.io/driver/mysql"
-	dsn := "root:@tcp(127.0.0.1:3306)/cmgostock?charset=utf8mb4&parseTime=True&loc=Local"
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := "root:@tcp(127.0.0.1:3306)/cmgostock?charset=utf8mb4&parseTime=True&loc=Local"
+	// database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// postgresql 	"gorm.io/driver/postgres"
+	// dsn := "user=postgres password=12341234 dbname=cmgostock port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+	// database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
--- go.mod
+++ go.mod
@@ -5,8 +5,9 @@ go 1.14
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	gorm.io/driver/mysql v1.0.1
+	gorm.io/driver/postgres v1.0.1
 	gorm.io/driver/sqlite v1.1.1
-	gorm.io/gorm v1.20.0
+	gorm.io/gorm v1.20.1
 )
