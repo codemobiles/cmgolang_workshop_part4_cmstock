5c6a292f6b3f57da44d8316082af9c8c205bc84c
codemobiles <chaiyasit.t@gmail.com>
2020-09-26 22:52:20 +0700

    deployment on hero - set default port

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"main/api"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,5 +13,13 @@ func main() {
 	router.Static("/images", "./uploaded/images")
 
 	api.Setup(router)
-	router.Run(":8081")
+
+	var port = os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("No Port In Heroku")
+		router.Run()
+	} else {
+		fmt.Println("Environment Port : " + port)
+		router.Run(port)
+	}
 }
