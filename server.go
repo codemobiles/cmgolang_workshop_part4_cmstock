808064e08f0bab979fbf9e3eac5ef67a85db4883
codemobiles <chaiyasit.t@gmail.com>
2020-09-27 09:10:14 +0700

    deployment on heroku again2

--- server.go
+++ server.go
@@ -13,10 +13,12 @@ func main() {
 	router.Static("/images", "./uploaded/images")
 
 	api.Setup(router)
+	// router.Run(":8081")
 
+	// In case of running on Heroku
 	var port = os.Getenv("PORT")
 	if port == "" {
-		fmt.Println("No Port In Heroku")
+		fmt.Println("Running on Heroku using random PORT")
 		router.Run()
 	} else {
 		fmt.Println("Environment Port : " + port)
